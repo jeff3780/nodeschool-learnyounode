44fcb6c5327c2d4b23b1fa096fa6d3e4c9d30110
Jeff Puuri <jeff@puuri.com>
2015-06-26 10:30:58 -0500

    Lesson 6 from learnyounode

--- program.js
+++ program.js
@@ -1,27 +1,50 @@
-var fs = require('fs');
-var path = require('path');
-
-fs.readdir( process.argv[2], function (err, files) { 
-	if (!err) {
-		//console.log(files);
-		for (var i = 0; i < files.length; i++) {
-			if (path.extname(files[i]).slice(1) === process.argv[3]) {
-				console.log(files[i]);
-			}
-			//console.log(files[i], path.extname(files[i]).slice(1));
-		}
+var mymodule = require('./mymodule');
+mymodule(process.argv[2], process.argv[3], function(err, list) {
+	if (err) return console.log('Error', err);
+	for (var i = 0; i < list.length; i++) {
+		console.log(list[i]);
 	}
-	
-});
-//console.log("Fired callback.");
+})
 
-//Official soln:
-/*    var fs = require('fs')
+/*Your solution to MAKE IT MODULAR passed!
+
+Here's the official solution in case you want to compare notes:
+
+───────────────────────────────────────────────────────────────
+
+solution.js:
+
+    var filterFn = require('./solution_filter.js')
+    var dir = process.argv[2]
+    var filterStr = process.argv[3]
+
+    filterFn(dir, filterStr, function (err, list) {
+      if (err)
+        return console.error('There was an error:', err)
+
+      list.forEach(function (file) {
+        console.log(file)
+      })
+    })
+
+───────────────────────────────────────────────────────────────
+
+solution_filter.js:
+
+    var fs = require('fs')
     var path = require('path')
 
-    fs.readdir(process.argv[2], function (err, list) {
-      list.forEach(function (file) {
-        if (path.extname(file) === '.' + process.argv[3])
-          console.log(file)
+    module.exports = function (dir, filterStr, callback) {
+
+      fs.readdir(dir, function (err, list) {
+        if (err)
+          return callback(err)
+
+        list = list.filter(function (file) {
+          return path.extname(file) === '.' + filterStr
+        })
+
+        callback(null, list)
       })
-    }) */
+    }
+*/
